7d701a64820f793921b1fd8ee2868ee234e92335
Fidan <aslanovafidan299@gmail.com>
2025-06-25 08:36:25 +0000

    first commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This module defines a Square class with a private size attribute."""
 
+
 class Square:
+    """A class that defines a square by its size."""
+
     def __init__(self, size):
+        """Initialize the square with a private size attribute."""
         self.__size = size
